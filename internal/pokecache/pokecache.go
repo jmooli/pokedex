2b81de586b829cbb46bb9ee00c30650ea300b502
jmooli <juha@mollerus.fi>
2025-02-03 21:13:23 +0200

    added internal module

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,75 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	entry map[string]cacheEntry
+	mu    sync.Mutex
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := Cache{
+		entry: make(map[string]cacheEntry),
+	}
+
+	cache.reaploop(interval)
+	return &cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.entry == nil {
+		c.entry = make(map[string]cacheEntry)
+	}
+
+	if _, exists := c.entry[key]; exists {
+		// propably not needed but error if key exists
+	}
+
+	c.entry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, exists := c.entry[key]
+
+	if !exists {
+		return nil, false
+	}
+
+	return c.entry[key].val, true
+}
+
+func (c *Cache) reaploop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.Clean(interval)
+	}
+}
+
+func (c *Cache) Clean(interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	now := time.Now()
+	for key, entry := range c.entry {
+		if now.Sub(entry.createdAt) > interval {
+			delete(c.entry, key)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,12 +1,165 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"github.com/jmooli/pokedex/internal/pokecache"
+	"io"
+	"net/http"
+	"os"
 	"strings"
 )
 
+var commands map[string]cliCommand
+
+type MapData struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type ApiResponse struct {
+	Count    int       `json:"count"`
+	Next     string    `json:"next"`
+	Previous string    `json:"previous"`
+	Results  []MapData `json:"results"`
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*Config) error
+}
+
+type Config struct {
+	Next     *string
+	Previous *string
+}
+
+func commandExit(c *Config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(c *Config) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, v := range commands {
+		fmt.Printf("%s: %s \n", v.name, v.description)
+	}
+	return nil
+}
+
+func commandMap(c *Config) error {
+	apiResp, err := makeApiGetRequest(*c.Next)
+	if err != nil {
+		return err
+	}
+	printOutAreas(apiResp)
+	c.Next = &apiResp.Next
+	c.Previous = &apiResp.Previous
+	return nil
+}
+func commandMapb(c *Config) error {
+	apiResp, err := makeApiGetRequest(*c.Previous)
+	if err != nil {
+		return err
+	}
+	printOutAreas(apiResp)
+	c.Next = &apiResp.Next
+	c.Previous = &apiResp.Previous
+	return nil
+}
+func makeApiGetRequest(url string) (ApiResponse, error) {
+	var apiResp ApiResponse
+
+	res, err := http.Get(url)
+	if err != nil {
+		return apiResp, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return apiResp, err
+	}
+
+	err = json.Unmarshal(data, &apiResp)
+	if err != nil {
+		return apiResp, err
+	}
+	return apiResp, nil
+}
+
+func printOutAreas(apiResp ApiResponse) {
+	for _, v := range apiResp.Results {
+		fmt.Printf("%s\n", v.Name)
+	}
+}
+
+func init() {
+
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Lists following 20 Maps",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "lists previous 20 Maps",
+			callback:    commandMapb,
+		},
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	config := Config{}
+
+	// init configuration with default values
+	initConfig(&config)
+
+	cache := Ca
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		command, exists := commands[cleanedInput[0]]
+		if exists {
+			err := command.callback(&config)
+			if err != nil {
+				fmt.Printf("Command error: %v\n", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+// should remove this, using the api to set next and previous @ command map and mapb
+func initConfig(c *Config) {
+	baseUrl := "https://pokeapi.co/api/v2/location-area/"
+	fullUrl := baseUrl + "?offset=0&limit=20"
+	c.Next = &fullUrl
 }
 
 func cleanInput(text string) []string {
